957dfad002379c17b7000f259f8535f08bac8115
Swapnil-Garg16 <swapnilgarg0@gmail.com>
2017-02-28 16:31:39 +0530

    Grid RecyclerView with BottomNavigationView

--- app/src/main/java/io/elpoisterio/smartlnmiit/Login.java
+++ app/src/main/java/io/elpoisterio/smartlnmiit/Login.java
@@ -1,13 +1,27 @@
 package io.elpoisterio.smartlnmiit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginBtn = (Button)findViewById(R.id.login);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this , MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
