d10194c2917933d479b42baf8605f08e63b8455a
Swapnil-Garg16 <swapnilgarg0@gmail.com>
2017-03-01 15:16:46 +0530

    Upload grades, class cancellation and chrome tabs.

---
+++ app/src/main/java/io/elpoisterio/smartlnmiit/activity/CancelClass.java
@@ -0,0 +1,24 @@
+package io.elpoisterio.smartlnmiit.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import io.elpoisterio.smartlnmiit.R;
+
+public class CancelClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancel_class);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
