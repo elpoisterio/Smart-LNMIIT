07411e2cae3b2e21b1604037444044d4e08a2deb
elpoisterio <rishabhyadav@aol.com>
2017-03-17 20:19:08 +0530

    check validity of email

---
+++ app/src/main/java/io/elpoisterio/smartlnmiit/utilities/HelperConstants.java
@@ -0,0 +1,26 @@
+package io.elpoisterio.smartlnmiit.utilities;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by rishabh on 17/3/17.
+ */
+
+public class HelperConstants {
+
+
+    public HelperConstants() {
+
+    }
+
+    public static boolean isEmailValid(CharSequence email) {
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        System.out.println(matcher);
+        return matcher.matches();
+    }
+
+}
