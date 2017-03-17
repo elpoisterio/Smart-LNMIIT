cdafae761211102de265b146b9afd75b123c758b
elpoisterio <rishabhyadav@aol.com>
2017-03-17 18:47:37 +0530

    added login method

--- app/src/main/java/io/elpoisterio/smartlnmiit/activity/Login.java
+++ app/src/main/java/io/elpoisterio/smartlnmiit/activity/Login.java
@@ -1,8 +1,14 @@
 package io.elpoisterio.smartlnmiit.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +18,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import io.elpoisterio.smartlnmiit.R;
+import io.elpoisterio.smartlnmiit.restClient.RestManager;
+import io.elpoisterio.smartlnmiit.utilities.CheckInternetConnection;
+import io.elpoisterio.smartlnmiit.utilities.HandlerConstant;
 
 public class Login extends AppCompatActivity implements View.OnClickListener{
 
@@ -19,12 +28,21 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
     EditText email;
     EditText password;
     Button signUpButton;
+    static Handler handler = new Handler();
+    ProgressDialog dialog;
+    private Context context = Login.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+        updateUi();
 
+
+
+    }
+    private void initView(){
         email = (EditText) findViewById(R.id.input_email);
         password = (EditText) findViewById(R.id.input_password);
         loginButton = (Button)findViewById(R.id.login);
@@ -32,6 +50,22 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
 
         loginButton.setOnClickListener(this);
         signUpButton.setOnClickListener(this);
+    }
+    private void updateUi(){
+
+        handler = new Handler(Looper.getMainLooper()){
+
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if(msg.what == HandlerConstant.FAILURE){
+                    Toast.makeText(Login.this,"Could not log in",Toast.LENGTH_SHORT).show();
+                }else {
+                    moveToHome();
+                }
+                hideDialog(dialog);
+            }
+        };
     }
 
     private boolean checkEmptyFields(){
@@ -63,13 +97,36 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
             if(!isEmailValid(email.getText().toString())){
                 Toast.makeText(Login.this, "Please use email id provided by college", Toast.LENGTH_SHORT).show();
             } else {
-                //callApi()
-                moveToHome();
+                if(!new CheckInternetConnection(Login.this).isConnectedToInternet()){
+                    Toast.makeText(context,"No internet connection",Toast.LENGTH_SHORT).show();
+                }else {
+                    callApi();
+                }
+
             }
 
         } else if(v == signUpButton){
             moveToSignUpScreen();
         }
+    }
+
+    private void callApi() {
+
+        dialog = new ProgressDialog(Login.this);
+        dialog.setCancelable(false);
+        dialog.setTitle("Logging in....");
+        dialog.show();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                new RestManager().getInstance().login(Login.this, email.getText().toString(), password.getText().toString(),handler);
+                Looper.loop();
+
+            }
+        });
+
     }
 
     private void moveToSignUpScreen() {
@@ -83,7 +140,22 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
         startActivity(intent);
         finish();
     }
-    private void saveDetailsToDB(){
 
+    private void hideDialog(ProgressDialog dialog) {
+        if (dialog != null && dialog.isShowing()) {
+            dialog.dismiss();
+            Log.i("HeyThere ", "hide dialog is running !!!");
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 }
