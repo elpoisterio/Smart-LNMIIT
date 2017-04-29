3dc362bff4db71d8847b77836cd5bca83ab198c4
elpoisterio <rishabhyadav@aol.com>
2017-04-29 21:30:54 +0530

    constants

--- app/src/main/java/io/elpoisterio/smartlnmiit/utilities/ApplicationConstant.java
+++ app/src/main/java/io/elpoisterio/smartlnmiit/utilities/ApplicationConstant.java
@@ -8,18 +8,21 @@ import android.content.Context;
 
 public class ApplicationConstant {
 
+    public static String url = "192.168.137.230:8080/";
     public static String header = "";
     public static String AUTH_TOKEN= "";
     public static String login ="";
-    public static String signUp = "";
+    public static String signUp = url+"signup";
     public  static String uploadGrade="";
     public static String cancelClass = "";
-    public static String uploadApplication="";
+    public static String uploadApplication=url +"createapp";
     public static String updateStatus = "";
     public static String getFacultyList="";
     public static String getUserProfile="";
     public static String updateProfile= "";
 
 
+    public static String postfirebaseKey= "";
+    public static String editProfile = url+"student/editProfile";
 
 }
