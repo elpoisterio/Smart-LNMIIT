137150fefe5d444aea7f3ee6a296d49fcf3d4f54
elpoisterio <rishabhyadav@aol.com>
2017-04-29 21:30:00 +0530

    upload grades

--- app/src/main/java/io/elpoisterio/smartlnmiit/activity/UploadGrades.java
+++ app/src/main/java/io/elpoisterio/smartlnmiit/activity/UploadGrades.java
@@ -1,18 +1,52 @@
 package io.elpoisterio.smartlnmiit.activity;
 
+import android.content.Intent;
+import android.content.IntentSender;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Result;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.DriveResource;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.OpenFileActivityBuilder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import io.elpoisterio.smartlnmiit.R;
 
 public class UploadGrades extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int ACTIVITY_CHOOSE_FILE = 1;
     Button uploadGrades;
     Button send;
     EditText courseName;
@@ -25,16 +59,85 @@ public class UploadGrades extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        uploadGrades = (Button) findViewById(R.id.upload_grades);
-        send = (Button) findViewById(R.id.send);
-        courseName = (EditText) findViewById(R.id.course_name);
-
+        initView();
 
 
     }
 
     @Override
-    public void onClick(View v) {
+    protected void onStart() {
+        super.onStart();
 
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+    }
+
+    private void initView() {
+
+        uploadGrades = (Button) findViewById(R.id.upload_grades);
+        send = (Button) findViewById(R.id.send);
+        courseName = (EditText) findViewById(R.id.course_name);
+        uploadGrades.setOnClickListener(this);
+        send.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == uploadGrades) {
+            openFolder();
+        }
+
+    }
+
+    private void openFolder() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
+        intent.setType("text/csv");
+        startActivityForResult(Intent.createChooser(intent, "Open CSV"), ACTIVITY_CHOOSE_FILE);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            File dir = Environment.getExternalStorageDirectory();
+            proImportCSV(new File(dir, data.getData().getPath()));
+
+        }
+    }
+
+    private void proImportCSV(File file) {
+
+        if (file != null){
+            System.out.println("YES");
+        }
+        byte[] byteArray = null;
+        try {
+            InputStream inputStream = new FileInputStream(file);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            byte[] b = new byte[1024 * 11];
+            int bytesRead = 0;
+
+            while ((bytesRead = inputStream.read(b)) != -1) {
+                bos.write(b, 0, bytesRead);
+            }
+
+            byteArray = bos.toByteArray();
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String base64 = Base64.encodeToString(byteArray, Base64.NO_WRAP);
+        System.out.println(base64);
+
+    }
+
+
 }
